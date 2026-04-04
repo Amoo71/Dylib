df88d90f03d4ea432f8523c0550ae3b7c804ecbf
Amoo71 <119591299+Amoo71@users.noreply.github.com>
2026-04-04 22:19:04 +0200

    Create VisionInspectApp.swift

---
+++ VisionInspectApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct VisionInspectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .preferredColorScheme(.dark)
+        }
+    }
+}
